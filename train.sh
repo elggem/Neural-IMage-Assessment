7c6b9a9674683ef82a499565e1e6320e95a19d95
elggem <trilly@gmail.com>
2020-07-02 15:58:13 +0200

    training script

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 
 python main.py --img_path "../GTTS/Samples" \
 --train \
---train_csv_file "../GTTS/Labels/pq_labels_all.csv" \
---val_csv_file "../GTTS/Labels/pq_labels_val.csv" \
+--train_csv_file "../GTTS/Labels/pqd_labels_train.csv" \
+--val_csv_file "../GTTS/Labels/pqd_labels_val.csv" \
 --conv_base_lr 3e-4 \
 --dense_lr 3e-3 \
 --decay \
