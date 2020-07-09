716f21e2d4fe2d885100050f6eabe109819bcdd6
elggem <trilly@gmail.com>
2020-07-09 17:58:29 +0200

    update analysis code

--- analysis/results.py
+++ analysis/results.py
@@ -5,35 +5,69 @@ from matplotlib.pyplot import imshow
 import matplotlib.pyplot as plt
 
 
+scenario = "pqd"
+
+
 # For running in Editor on Desktop
 # cd /Users/ralf/Documents/github/_TUBForks/Neural-IMage-Assessment
 path_drive_prefix = "../GTTS/"
-path_patch_distribution_labels = path_drive_prefix + "Labels/pqd_labels_test.csv"
-path_patch_distribution_predictions = "./tests_pqd/epoch-28_test_pred.csv"
+path_patch_distribution_labels = path_drive_prefix + "Labels/%s_labels_test.csv" % (scenario)
 
 sample_names = np.genfromtxt(path_patch_distribution_labels,delimiter=",",dtype=str,encoding='utf-8',usecols=(0)).tolist()
 gt_means = np.genfromtxt(path_patch_distribution_labels,delimiter=",",usecols=(1))
 gt_stds = np.genfromtxt(path_patch_distribution_labels,delimiter=",",usecols=(2))
 gt_pqds = np.genfromtxt(path_patch_distribution_labels,delimiter=",")[:,3:]
 
-
-path = "./results/001-not-normed/tests_pqd/epoch-50_test_pred.csv"
+path = "./tests_%s/epoch-34_test_pred.csv" % (scenario)
 pred_means = np.genfromtxt(path,delimiter=",",usecols=(1))
 pred_stds = np.genfromtxt(path,delimiter=",",usecols=(2))
 pred_pqds = np.genfromtxt(path,delimiter=",")[:,3:]
 
 # Basic Plots
-sample_names[100]
-plt.plot(gt_pqds[100])
-plt.plot(pred_pqds[100])
+i = 120
+sample_names[i]
+plt.plot(gt_pqds[i]/gt_pqds[i].sum()); plt.plot(pred_pqds[i])
+
 gt_pqds[0].sum()
 pred_pqds[0].sum()
 
-# Mean Std plots
-plt.rcParams.update({'figure.figsize':(10,10), 'figure.dpi':50})
-plt.gca().set(title="Mean vs Standard Deviation predictions"); plt.xlabel("mean patch quality"); plt.ylabel("standard deviation patch quality"); plt.scatter(pred_means,pred_stds)
+#VMAF
+subset_samples = np.genfromtxt(path_scores_labels_subset,delimiter=",",dtype=str,encoding='utf-8',usecols=(0)).tolist()
+path_scores_labels_subset = path_drive_prefix + "Labels/ParsedVMAF_subset.csv"
+subset_scores = np.genfromtxt(path_scores_labels_subset,delimiter=",",usecols=(1))
+vmaf_scores = []
+for sample in sample_names:
+    vmaf_score = subset_scores[subset_samples.index(sample)]
+    vmaf_scores.append(vmaf_score)
+vmaf_scores = np.array(vmaf_scores)
+plt.gca().set(title="VMAF distribution of samples", ylabel='Frequency'); plt.hist(vmaf_scores, bins=100); plt.savefig('vmaf_distribution_original_labels.png')
 
-plt.gca().set(title="Mean vs Standard Deviation ground truth"); plt.xlabel("mean patch quality"); plt.ylabel("standard deviation patch quality"); plt.scatter(gt_means,gt_stds)
+#Resolution
+resolution_tags = []
+for sample in sample_names:
+    for i, resolution in enumerate(['640', '1280', '1920']):
+        if resolution in sample:
+            resolution_tags.append(i)
+
+# Bitrates
+bitrate_tags = []
+for sample in sample_names:
+    bitrate_tags.append(int(sample.split("_")[-8]))
+bitrate_tags = np.array(bitrate_tags)
+
+# Mean Std plots
+plt.rcParams.update({'figure.figsize':(5,5), 'figure.dpi':100})
+plt.gca().set(title="Mean vs Standard Deviation predictions"); plt.xlabel("mean patch quality"); plt.ylabel("standard deviation patch quality"); plt.scatter(pred_means,pred_stds, c=resolution_tags)
+
+plt.gca().set(title="Mean vs Standard Deviation ground truth"); plt.xlabel("mean patch quality"); plt.ylabel("standard deviation patch quality"); plt.scatter(gt_means,gt_stds, c=resolution_tags)
+
+plt.rcParams.update({'figure.figsize':(10,10), 'figure.dpi':70})
+plt.gca().set(title="Mean vs Standard Deviation predictions"); plt.xlabel("mean patch quality"); plt.ylabel("standard deviation patch quality"); plt.xlim(0,120); plt.ylim(0,25); plt.scatter(pred_means,pred_stds, c=resolution_tags, alpha = 0.5, marker='o'); plt.scatter(gt_means,gt_stds, c=resolution_tags, alpha = 0.5, marker='+')
+
+
+plt.gca().set(title="distribution of standard deviations, red=predicted, blue=gt", ylabel='Frequency'); plt.hist(pred_stds, bins=100, color='red', range=(0,30), alpha = 0.5); plt.hist(gt_stds, bins=100, color='blue', range=(0,30), alpha = 0.5)
+
+plt.gca().set(title="distribution of standard deviations, red=predicted, blue=gt", ylabel='Frequency'); plt.ylim(0,25); plt.hist(pred_means, bins=100, color='red', range=(0,150), alpha = 0.5); plt.hist(gt_means, bins=100, color='blue', range=(0,150), alpha = 0.5)
 
 
 # Experimental Results for low/medium/high resolution/bitrate MSE
@@ -70,4 +104,4 @@ for i, experiment in enumerate(experiments):
             N += 1
             MSE += (gt_means[sample_names.index(sample)] - pred_means[sample_names.index(sample)]) ** 2
     MSE /= N
-    print("Experiment number %02d, N=%03d, MSE=%.4f" % (i, N, MSE))
+    print("Experiment number %02d, N, %03d, MSE, %.4f" % (i, N, MSE))
