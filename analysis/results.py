205f8d757d8cfac13037d3db2fac1c4869f04eb5
elggem <trilly@gmail.com>
2020-07-28 15:00:18 +0200

    update for final results

--- analysis/results.py
+++ analysis/results.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import imshow
 import matplotlib.pyplot as plt
 
 
-scenario = "pqd"
+scenario = "anf"
 
 
 # For running in Editor on Desktop
@@ -19,7 +19,7 @@ gt_means = np.genfromtxt(path_patch_distribution_labels,delimiter=",",usecols=(1
 gt_stds = np.genfromtxt(path_patch_distribution_labels,delimiter=",",usecols=(2))
 gt_pqds = np.genfromtxt(path_patch_distribution_labels,delimiter=",")[:,3:]
 
-path = "./tests_%s/epoch-34_test_pred.csv" % (scenario)
+path = "./results/003-run/tests_%s/epoch-24_test_pred.csv" % (scenario)
 pred_means = np.genfromtxt(path,delimiter=",",usecols=(1))
 pred_stds = np.genfromtxt(path,delimiter=",",usecols=(2))
 pred_pqds = np.genfromtxt(path,delimiter=",")[:,3:]
@@ -132,3 +132,33 @@ for i, experiment in enumerate(experiments):
             MSE += (gt_means[sample_names.index(sample)] - pred_means[sample_names.index(sample)]) ** 2
     MSE /= N
     print("Experiment number %02d, N, %03d, MSE, %.4f" % (i, N, MSE))
+
+## results as MSE of each experiment
+experimentkeys = ["00", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+resultkeys = ["PQD1","PQD2","ANV1","ANV2","ANF1","ANF2"]
+results = np.array([[110.5937,122.0417,121.9868,117.2077,115.4500,120.2718],
+[77.5845,84.9238,85.6827,77.9098,74.3035,94.1044],
+[111.5058,103.0548,109.3072,110.5940,115.5720,113.9274],
+[140.6993,178.9930,170.8900,161.9356,154.1484,152.2945],
+[63.1799,72.6795,83.2724,81.6832,68.9677,86.7477],
+[106.9395,104.3602,110.1706,100.9691,109.4638,109.0861],
+[149.2010,179.6596,164.6004,163.0855,156.3169,158.3697],
+[54.0787,77.9493,75.7079,77.7112,65.1996,89.4305],
+[99.6833,87.4642,99.2993,94.3899,108.3991,101.0227],
+[159.5384,215.9231,203.5903,192.5412,174.0614,176.3784]])
+
+
+pqd_mean = np.mean(results[:,(0,1)], axis=1)
+pqd_std = np.abs(results[:,1] - results[:,0])
+
+anv_mean = np.mean(results[:,(2,3)], axis=1)
+anv_std = np.abs(results[:,2] - results[:,3])
+
+anf_mean = np.mean(results[:,(4,5)], axis=1)
+anf_std = np.abs(results[:,4] - results[:,5])
+
+x = np.arange(len(experimentkeys))
+width = 0.25
+
+plt.rcParams.update({'figure.figsize':(18,10), 'figure.dpi':100})
+plt.ylabel("MSE"); plt.xlabel("experiment"); plt.title("MSE of predictions for 2 seperately trained models on testset"); plt.xticks(x); plt.bar(x - width, pqd_mean, width, yerr=pqd_std, label="PQD"); plt.bar(x , anv_mean, width, yerr=anv_std, label="ANV"); plt.bar(x+ width, anf_mean, width, yerr=anf_std, label="ANF");  plt.legend(loc='upper left'); plt.savefig("/Users/ralf/Desktop/MSEAll.png")
--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 python test.py --test_images "../GTTS/Samples" \
---test_csv "../GTTS/Labels/$1_labels_test.csv" \
---model ./checkpoints_$1/epoch-37.pth \
---predictions ./tests_$1/
+--test_csv "../GTTS/Labels/pqd_labels_test.csv" \
+--model ./results/002-run2/checkpoints_pqd/epoch-15.pth \
+--predictions ./results/002-run2/tests_pqd/
+
+python test.py --test_images "../GTTS/Samples" \
+--test_csv "../GTTS/Labels/anv_labels_test.csv" \
+--model ./results/002-run2/checkpoints_anv/epoch-28.pth \
+--predictions ./results/002-run2/tests_anv/
+
+python test.py --test_images "../GTTS/Samples" \
+--test_csv "../GTTS/Labels/anf_labels_test.csv" \
+--model ./results/002-run2/checkpoints_anf/epoch-24.pth \
+--predictions ./results/002-run2/tests_anf/
